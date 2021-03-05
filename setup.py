202623641026e9ec17404c8e86af65bfcf95de26
Erik Sundell <erik.i.sundell@gmail.com>
2021-03-05 01:29:40 +0100

    Bump to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jupyterhub-idle-culler",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     license="3-BSD",
     long_description=open("README.md").read(),
