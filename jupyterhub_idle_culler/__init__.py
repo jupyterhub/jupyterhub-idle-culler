5694680f6cac8fa13dff978c4a270c972114d258
Erik Sundell <erik@sundellopensource.se>
2024-02-26 09:17:29 +0100

    Bump to 1.3.1.dev

--- jupyterhub_idle_culler/__init__.py
+++ jupyterhub_idle_culler/__init__.py
@@ -20,7 +20,7 @@ from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado.log import app_log
 from tornado.options import define, options, parse_command_line
 
-__version__ = "1.3.0"
+__version__ = "1.3.1.dev"
 
 STATE_FILTER_MIN_VERSION = V("1.3.0")
 
